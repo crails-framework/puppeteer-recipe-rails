a2dbc1811195e96aba87f2994d21756da660c0d8
Michael <michael@unetresgrossebite.com>
2020-02-29 12:41:22 +0100

    split environment variables files into env and env.puppet

--- setup/05_rails.sh
+++ setup/05_rails.sh
@@ -5,8 +5,12 @@ source ./variables 2> /dev/null
 mkdir -p "$APP_PATH"
 mkdir -p "$APP_PATH/runtime"
 
-bash rails.env   > "$APP_PATH/env"
+bash rails.env   > "$APP_PATH/env.puppet"
 cp rails.launch "$APP_PATH/runtime/launch.sh"
 cp rails.stop   "$APP_PATH/runtime/stop.sh"
+
+if [ ! -f "$APP_PATH/env" ] ; then
+  bash default.env > "$APP_PATH/env"
+fi
 
 chmod +x "$APP_PATH/runtime/"{launch,stop}.sh
