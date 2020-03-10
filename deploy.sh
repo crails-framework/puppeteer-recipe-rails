18a16a1c99e7978dbdb41870198f5f1ab3374d94
Michael <michael@unetresgrossebite.com>
2020-03-10 15:58:42 +0100

    deploy file now gets rid of older sprockets-manifest files

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 source ./variables 2> /dev/null
 
 cd "$APP_PATH"/runtime
+echo "- removing old sprockets-manifest file"
+rm public/assets/.sprockets-manifest-*.json || echo "-- no older sprockets-manifest file found"
 tar -xf "$BUILD_TARBALL"
 
 chown -R "$APP_USER":"$APP_USER" "$APP_PATH"/runtime
