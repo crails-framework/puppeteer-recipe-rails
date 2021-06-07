21a1a04809bdf027fda18f4d423f1d6f764d9258
Michael <michael@unetresgrossebite.com>
2021-06-07 13:16:01 +0200

    implements RAILS_SEED_ON_DEPLOY option

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,7 @@ chown -R "$APP_USER":"$APP_USER" "$APP_PATH"/runtime
 
 su "$APP_USER" -c ". ../env && bundle install --path vendor/bundle"
 su "$APP_USER" -c ". ../env && bundle exec rake db:migrate"
+
+if [[ "$RAILS_SEED_ON_DEPLOY" == "1" ]] ; then
+  su "$APP_USER" -c ". ../env && bundle exec rake db:seed"
+fi
