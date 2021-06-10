7d0e3b2c1e3481a7de527ff52042e57afba14c0c
Michael <michael@unetresgrossebite.com>
2021-06-10 04:25:47 +0200

    update logreader.sh

--- logreader.sh
+++ logreader.sh
@@ -5,6 +5,14 @@ cd "$APP_PATH/runtime"
 source ../env 2> /dev/null
 
 logfile="$APP_PATH/runtime/log/$RAILS_ENV.log"
+total_lines=`wc -l "$logfile" | cut -d' ' -f1`
+extract_lines=500
 
-wc -l "$logfile" | cut -d' ' -f1
-tail -n 500 "$logfile"
+if [[ "$LAST_LOG_LINE" > "$total_lines" ]] ; then
+  extract_lines=`$total_lines`
+elif [[ "$LAST_LOG_LINE" > 0 ]] ; then
+  extract_lines=`expr $total_lines - $LAST_LOG_LINE`
+fi
+
+echo $total_lines
+tail -n $extract_lines "$logfile"
